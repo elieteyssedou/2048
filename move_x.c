57b8d666e70aac4a8f84622036c1821a5c11dced
Elie <elieteyssedou@gmail.com>
2015-02-28 18:19:27 +0100

    merge

--- move_x.c
+++ move_x.c
@@ -22,14 +22,12 @@ void	move_left(int tab[4][4])
 	{
 		x = -1;
 		while (++x < 4)
-		{
 			if (x < 1 && tab[y][x + 1] == 0 && tab[y][x + 2] == 0 && tab[y][x] == tab[y][x + 3])
 				tab[y][x] *= 2, tab[y][x + 3] = 0;
 			else if (x < 2 && tab[y][x + 1] == 0 && tab[y][x] == tab[y][x + 2])
 				tab[y][x] *= 2, tab[y][x + 2] = 0;
 			else if (x != 3 && tab[y][x] == tab[y][x + 1])
 				tab[y][x] *= 2, tab[y][x + 1] = 0;
-		}
 		move_l(tab, y), move_l(tab, y), move_l(tab, y);
 	}
 }
@@ -40,13 +38,8 @@ void	move_l(int tab[4][4], int y)
 
 	x = 0;
 	while (++x < 4)
-	{
 		if (tab[y][x - 1] == 0)
-		{
-			tab[y][x - 1] = tab[y][x];
-			tab[y][x] = 0;
-		}
-	}
+			tab[y][x - 1] = tab[y][x], tab[y][x] = 0;
 }
 
 void	move_right(int tab[4][4])
@@ -59,14 +52,12 @@ void	move_right(int tab[4][4])
 	{
 		x = 4;
 		while (--x > -1)
-			{
 				if (x > 2 && tab[y][x - 1] == 0 && tab[y][x - 2] == 0 && tab[y][x] == tab[y][x - 3])
 					tab[y][x] *= 2, tab[y][x - 3] = 0;
 				else if (x > 1 && tab[y][x - 1] == 0 && tab[y][x] == tab[y][x - 2])
 					tab[y][x] *= 2, tab[y][x - 2] = 0;
 				else if (x != 0 && tab[y][x] == tab[y][x - 1])
 					tab[y][x] *= 2, tab[y][x - 1] = 0;
-			}
 		move_r(tab, y), move_r(tab, y), move_r(tab, y);
 	}
 }
