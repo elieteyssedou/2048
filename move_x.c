12aea7b88594c3dc33b2fc51e708c017dd9bbcac
Elie <elieteyssedou@gmail.com>
2015-02-28 18:31:52 +0100

    norme moves

--- move_x.c
+++ move_x.c
@@ -22,9 +22,11 @@ void	move_left(int tab[4][4])
 	{
 		x = -1;
 		while (++x < 4)
-			if (x < 1 && tab[y][x + 1] == 0 && tab[y][x + 2] == 0 && tab[y][x] == tab[y][x + 3])
+			if (x < 1 && tab[y][x + 1] == 0 && tab[y][x + 2] == 0
+				&& tab[y][x] == tab[y][x + 3])
 				tab[y][x] *= 2, tab[y][x + 3] = 0;
-			else if (x < 2 && tab[y][x + 1] == 0 && tab[y][x] == tab[y][x + 2])
+			else if (x < 2 && tab[y][x + 1] == 0
+				&& tab[y][x] == tab[y][x + 2])
 				tab[y][x] *= 2, tab[y][x + 2] = 0;
 			else if (x != 3 && tab[y][x] == tab[y][x + 1])
 				tab[y][x] *= 2, tab[y][x + 1] = 0;
@@ -39,7 +41,10 @@ void	move_l(int tab[4][4], int y)
 	x = 0;
 	while (++x < 4)
 		if (tab[y][x - 1] == 0)
-			tab[y][x - 1] = tab[y][x], tab[y][x] = 0;
+		{
+			tab[y][x - 1] = tab[y][x];
+			tab[y][x] = 0;
+		}
 }
 
 void	move_right(int tab[4][4])
@@ -52,12 +57,14 @@ void	move_right(int tab[4][4])
 	{
 		x = 4;
 		while (--x > -1)
-				if (x > 2 && tab[y][x - 1] == 0 && tab[y][x - 2] == 0 && tab[y][x] == tab[y][x - 3])
-					tab[y][x] *= 2, tab[y][x - 3] = 0;
-				else if (x > 1 && tab[y][x - 1] == 0 && tab[y][x] == tab[y][x - 2])
-					tab[y][x] *= 2, tab[y][x - 2] = 0;
-				else if (x != 0 && tab[y][x] == tab[y][x - 1])
-					tab[y][x] *= 2, tab[y][x - 1] = 0;
+			if (x > 2 && tab[y][x - 1] == 0 && tab[y][x - 2] == 0
+				&& tab[y][x] == tab[y][x - 3])
+				tab[y][x] *= 2, tab[y][x - 3] = 0;
+			else if (x > 1 && tab[y][x - 1] == 0
+				&& tab[y][x] == tab[y][x - 2])
+				tab[y][x] *= 2, tab[y][x - 2] = 0;
+			else if (x != 0 && tab[y][x] == tab[y][x - 1])
+				tab[y][x] *= 2, tab[y][x - 1] = 0;
 		move_r(tab, y), move_r(tab, y), move_r(tab, y);
 	}
 }
@@ -69,5 +76,8 @@ void	move_r(int tab[4][4], int y)
 	x = 3;
 	while (--x > -1)
 		if (tab[y][x + 1] == 0)
-			tab[y][x + 1] = tab[y][x], tab[y][x] = 0;
+		{
+			tab[y][x + 1] = tab[y][x];
+			tab[y][x] = 0;
+		}
 }
